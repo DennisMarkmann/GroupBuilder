c56ab7494a23455fae1757d67fbf3ee097713dd5
dennis.markmann <dennismarkmann@hotmail.de>
2013-06-28 13:20:16 +0200

    Added aboutFrame text.

--- GroupBuilder/src/com/kn/groupBuilder/Gui/Menu/AboutFrame.java
+++ GroupBuilder/src/com/kn/groupBuilder/Gui/Menu/AboutFrame.java
@@ -18,9 +18,12 @@ public final class AboutFrame extends JFrame implements DefaultFrame {
     private AboutFrame(final Pojo pojo) {
 
         BUILDER.setDefaultFrameSettings(this, "About");
+        this.setSize(600, 230);
         this.addWindowListener(new MyWindowAdapter(this));
-        BUILDER.createLabel(this, "Designed by Dennis Markmann", 1, 2);
-        final JButton closeButton = BUILDER.createButton(this, "closeButton", "close", 0, 3);
+        BUILDER.createLabel(this, "This Software is designed by Dennis Markmann.", 1, 0);
+        BUILDER.createLabel(this, "If you have any problems or suggested improvements please contanct me via E-Mail.", 1, 1);
+        BUILDER.createLabel(this, "Address: dennismarkmann@hotmail.de", 1, 2);
+        final JButton closeButton = BUILDER.createButton(this, "closeButton", "close", 2, 4);
 
         final ActionListener listener = new AboutFrameListener(this);
 
