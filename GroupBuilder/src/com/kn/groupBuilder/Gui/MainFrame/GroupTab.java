01b04328c7d6d3ad4694bb0d7020a9b6e547e111
it1-markde <it1-markde@R053-07.g18.de>
2013-09-16 10:59:03 +0200

    Updated some shit and started the implementation of the tableSort functionality.

--- GroupBuilder/src/com/kn/groupBuilder/Gui/MainFrame/GroupTab.java
+++ GroupBuilder/src/com/kn/groupBuilder/Gui/MainFrame/GroupTab.java
@@ -5,16 +5,21 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 
 import com.kn.groupBuilder.Gui.MainFrame.Listener.GroupTabListener;
 import com.kn.groupBuilder.Gui.TableModels.GroupTableModel;
+import com.kn.groupBuilder.Gui.TableModels.Implementations.JTableButtonMouseListener;
+import com.kn.groupBuilder.Gui.TableModels.Implementations.JTableButtonRenderer;
 import com.kn.groupBuilder.Storage.Pojo;
 
 import dennis.markmann.MyLibraries.GuiJobs.DefaultFrames.Implementations.DefaultTab;
 
 /**
- * Second tab of the GUI. Used to access many other operations and to see the different groups via table.
+ * Second tab of the GUI. Used to access many other operations and to see the
+ * different groups via table.
  * 
  * @author dennis.markmann
  * @since JDK.1.7.0_21
@@ -23,26 +28,48 @@ import dennis.markmann.MyLibraries.GuiJobs.DefaultFrames.Implementations.Default
 
 class GroupTab extends JPanel implements DefaultTab {
 
-    private static final long serialVersionUID = 1673516265342795696L;
+	private static final long serialVersionUID = 1673516265342795696L;
 
-    GroupTab(final Pojo pojo) {
+	GroupTab(final Pojo pojo) {
 
-        final ArrayList<String> buttonRenderCols = new ArrayList<String>();
-        buttonRenderCols.add("Edit");
-        buttonRenderCols.add("Remove");
+		final ArrayList<String> buttonRenderCols = new ArrayList<String>();
+		buttonRenderCols.add("Edit");
+		buttonRenderCols.add("Remove");
 
-        BUILDER.setDefaultTabSettings(this);
-        BUILDER.createTable(this, 0, 0, new JTable(GroupTableModel.createTable(pojo)), buttonRenderCols);
+		BUILDER.setDefaultTabSettings(this);
 
-        BUILDER.getGridBagConstraints().fill = GridBagConstraints.NONE;
-        final JButton addButton = BUILDER.createButton(this, "addButton", "Add Group", 0, 5);
-        final JButton createGroupsButton = BUILDER.createButton(this, "createGroupsButton", "Create Groups", 0, 6);
-        final JButton saveButton = BUILDER.createButton(this, "saveButton", "Save", 0, 7);
+		//
+		final JTable table = new JTable(GroupTableModel.createTable(pojo));
+		final TableCellRenderer buttonRenderer = new JTableButtonRenderer();
 
-        final GroupTabListener listener = new GroupTabListener(pojo);
+		for (final String rowName : buttonRenderCols) {
+			table.getColumn(rowName).setCellRenderer(buttonRenderer);
+		}
+		table.addMouseListener(new JTableButtonMouseListener(table));
+		table.getTableHeader().addMouseListener(
+				new JTableButtonMouseListener(table));
 
-        addButton.addActionListener(listener);
-        createGroupsButton.addActionListener(listener);
-        saveButton.addActionListener(listener);
-    }
+		final JScrollPane scrollPane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
+
+		BUILDER.setPosition(this, BUILDER.getGridBagConstraints(), 0, 0,
+				scrollPane);
+
+		// BUILDER.createTable(this, 0, 0,
+		// new JTable(GroupTableModel.createTable(pojo)), buttonRenderCols);
+
+		BUILDER.getGridBagConstraints().fill = GridBagConstraints.NONE;
+		final JButton addButton = BUILDER.createButton(this, "addButton",
+				"Add Group", 0, 5);
+		final JButton createGroupsButton = BUILDER.createButton(this,
+				"createGroupsButton", "Create Groups", 0, 6);
+		final JButton saveButton = BUILDER.createButton(this, "saveButton",
+				"Save", 0, 7);
+
+		final GroupTabListener listener = new GroupTabListener(pojo);
+
+		addButton.addActionListener(listener);
+		createGroupsButton.addActionListener(listener);
+		saveButton.addActionListener(listener);
+	}
 }
---
+++ GroupBuilder/src/com/kn/groupBuilder/Gui/TableModels/Implementations/JTableButtonMouseListener.java
@@ -0,0 +1,45 @@
+package com.kn.groupBuilder.Gui.TableModels.Implementations;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JTable;
+
+/**
+ * MouseListener to enable mouse events on JTables.
+ * 
+ * @author dennis.markmann
+ * @since jdk1.7.0_21
+ * @version 1.0
+ */
+
+public class JTableButtonMouseListener extends MouseAdapter {
+
+	private final JTable table;
+
+	public JTableButtonMouseListener(final JTable table) {
+		this.table = table;
+	}
+
+	@Override
+	public final void mouseClicked(final MouseEvent e) {
+		final int column = this.table.getColumnModel().getColumnIndexAtX(
+				e.getX());
+		final int row = e.getY() / this.table.getRowHeight();
+		final int index = this.table.columnAtPoint(e.getPoint());
+
+		if (index >= 0) {
+			// TODO
+			// System.out.println(this.table.getColumnName(index));
+		}
+
+		if (row < this.table.getRowCount() && row >= 0
+				&& column < this.table.getColumnCount() && column >= 0) {
+			final Object value = this.table.getValueAt(row, column);
+			if (value instanceof JButton) {
+				((JButton) value).doClick();
+			}
+		}
+	}
+}
