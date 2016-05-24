dbd8e3d5f44a15f2b89d348dcb32f0d801ddc99e
Dennis Markmann <dennismarkmann@hotmail.de>
2016-05-24 10:57:05 +0200

    Umstellung auf Java 1.8.

--- GroupBuilder/src/markmann/dennis/groupBuilder/exceptions/ExceptionDialogInterface.java
+++ GroupBuilder/src/markmann/dennis/groupBuilder/exceptions/ExceptionDialogInterface.java
@@ -2,9 +2,8 @@ package markmann.dennis.groupBuilder.exceptions;
 
 /**
  * Interface for the implementation of the showDialog method for all exceptions.
- * 
- * @author it1-markde
- * @since jdk1.7.0_21
+ *
+ * @author dennis.markmann
  */
 
 interface ExceptionDialogInterface {
--- GroupBuilder/src/markmann/dennis/groupBuilder/gui/tableModels/listener/JTableButtonMouseListener.java
+++ GroupBuilder/src/markmann/dennis/groupBuilder/gui/tableModels/listener/JTableButtonMouseListener.java
@@ -8,33 +8,30 @@ import javax.swing.JTable;
 
 /**
  * MouseListener to enable mouse events on JTables.
- * 
+ *
  * @author dennis.markmann
- * @since jdk1.7.0_21
  * @version 1.0
  */
 
 public class JTableButtonMouseListener extends MouseAdapter {
 
-	private final JTable table;
+    private final JTable table;
 
-	public JTableButtonMouseListener(final JTable table) {
+    public JTableButtonMouseListener(final JTable table) {
 
-		this.table = table;
-	}
+        this.table = table;
+    }
 
-	@Override
-	public final void mouseClicked(final MouseEvent e) {
-		final int column = this.table.getColumnModel().getColumnIndexAtX(
-				e.getX());
-		final int row = e.getY() / this.table.getRowHeight();
+    @Override
+    public final void mouseClicked(final MouseEvent e) {
+        final int column = this.table.getColumnModel().getColumnIndexAtX(e.getX());
+        final int row = e.getY() / this.table.getRowHeight();
 
-		if (row < this.table.getRowCount() && row >= 0
-				&& column < this.table.getColumnCount() && column >= 0) {
-			final Object value = this.table.getValueAt(row, column);
-			if (value instanceof JButton) {
-				((JButton) value).doClick();
-			}
-		}
-	}
+        if (row < this.table.getRowCount() && row >= 0 && column < this.table.getColumnCount() && column >= 0) {
+            final Object value = this.table.getValueAt(row, column);
+            if (value instanceof JButton) {
+                ((JButton) value).doClick();
+            }
+        }
+    }
 }
