5b79f4a13bb8a52835a8610af8bb8a2b2b945209
Dennis Markmann <dennismarkmann@hotmail.de>
2016-07-22 13:19:02 +0200

    Code improvements.

--- GroupBuilder/src/markmann/dennis/groupBuilder/storage/Language.java
+++ GroupBuilder/src/markmann/dennis/groupBuilder/storage/Language.java
@@ -1,5 +1,21 @@
 package markmann.dennis.groupBuilder.storage;
 
-public class Language {
-    // TODO implement enum to replace languagelist in pojo.
+public enum Language {
+    // TODO use to replace languagelist in pojo / language in settings.
+    ENGLISH {
+
+        @Override
+        public String toString() {
+            return "English";
+        }
+    },
+
+    GERMAN {
+
+        @Override
+        public String toString() {
+            return "German";
+        }
+    };
+
 }
